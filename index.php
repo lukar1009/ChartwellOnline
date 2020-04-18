39c1c65ee31687f934a684a08c210cce3f2fc702
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-18 16:32:41 +0200

    Added login page to website

--- index.php
+++ index.php
@@ -1,45 +1,45 @@
-<?php require "includes/header.php"; ?>
-<?php require "includes/navigation.php"; ?>
+<?php require "./includes/db.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>[Chartwell] Online Classes</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+    <link rel="stylesheet" href="./css/login_style.css">
+</head>
+<body>
 
-<div class="container-fluid">
-
-<div class="row">
-    <div class="col-lg-6 pl-3 pr-3 pt-3">
-        <?php require "includes/slider.php"; ?>
-    </div>
-    <div class="col-lg-6 pt-3">
-        <!-- <h1 class="text-wrap text-center text-light font-weight-light font-italic">Welcome to Chartwell ONLINE CLASSROOM</h1> -->
-        <!-- <h1 class="text-light display-4 text-center font-italic">Welcome to Chartwell ONLINE CLASSROOM</h1>
-        <p class="mt-4 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas obcaecati, similique voluptatum quos, ratione laudantium optio dolor eveniet suscipit provident sit. Ea reiciendis tenetur enim omnis veniam totam amet excepturi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis nulla assumenda, at eum ipsam sit corporis vel fugit odio hic, sint doloribus cum! Modi cum nostrum nobis commodi aut?</p> -->
-        <div class="jumbotron mb-0"style="height: 500px;">
-            <h1 class="display-4">Hello, Username!</h1>
-            <p class="lead text-justify">Welcome to Chartwell Online Classroom! This is made for all of you pupils who want to take your learning to a next level!</p>
-            <hr class="my-4">
-            <p class="text-justify">Please follow the instructions made by your teachers, and do not attempt to do any illegal stuff.</p>
-            <p class="text-justify">For any further information about Chartwell or contact, please click on the button bellow for Chartwell official website.</p>
-            <a class="btn btn-danger btn-lg" href="https://chartwell.edu.rs/" role="button">Chartwell Official Website</a>
+<div class="modal-dialog text-center">
+    <div class="col-sm-9 main-section">
+        <div class="modal-content">
+            <div class="col-12 user-img">
+                <a href="https://chartwell.edu.rs/">
+                    <img src="images/chartwell.png" alt="">
+                </a>
+            </div>
+            <div class="col-12 form-input">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Enter Username:">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Enter Username:">
+                    </div>
+                    <button class="btn btn-success">Login!</button>
+                </form>
+            </div>
+            <div class="col-12 websitelink">
+                <a href="https://chartwell.edu.rs/">Go back to Chartwell Website.</a>
+            </div>
         </div>
     </div>
-    <div class="col-lg-12 pl-3 pr-3">
-        <hr class="bg-white">
-        <p class="text-center text-white font-italic">Chartwell International School, Belgrade, Serbia</p>
-    </div>
-</div>
-
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-<?php require "includes/footer.php"; ?>
-    
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="./js/main.js"></script>
+</body>
+</html>
