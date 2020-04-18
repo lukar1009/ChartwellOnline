92be1d08c8b19127f71cddf7e929031b4c411812
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-18 15:32:53 +0200

    First version of home page done.

--- index.php
+++ index.php
@@ -4,20 +4,25 @@
 <div class="container-fluid">
 
 <div class="row">
-    <div class="col-lg-6 pl-3 pr-3 pt-1">
+    <div class="col-lg-6 pl-3 pr-3 pt-3">
         <?php require "includes/slider.php"; ?>
     </div>
-    <div class="col-lg-6 pt-1">
+    <div class="col-lg-6 pt-3">
         <!-- <h1 class="text-wrap text-center text-light font-weight-light font-italic">Welcome to Chartwell ONLINE CLASSROOM</h1> -->
         <!-- <h1 class="text-light display-4 text-center font-italic">Welcome to Chartwell ONLINE CLASSROOM</h1>
         <p class="mt-4 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas obcaecati, similique voluptatum quos, ratione laudantium optio dolor eveniet suscipit provident sit. Ea reiciendis tenetur enim omnis veniam totam amet excepturi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis nulla assumenda, at eum ipsam sit corporis vel fugit odio hic, sint doloribus cum! Modi cum nostrum nobis commodi aut?</p> -->
-        <div class="jumbotron"style="height: 500px;">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <div class="jumbotron mb-0"style="height: 500px;">
+            <h1 class="display-4">Hello, Username!</h1>
+            <p class="lead text-justify">Welcome to Chartwell Online Classroom! This is made for all of you pupils who want to take your learning to a next level!</p>
             <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <p class="text-justify">Please follow the instructions made by your teachers, and do not attempt to do any illegal stuff.</p>
+            <p class="text-justify">For any further information about Chartwell or contact, please click on the button bellow for Chartwell official website.</p>
+            <a class="btn btn-danger btn-lg" href="https://chartwell.edu.rs/" role="button">Chartwell Official Website</a>
         </div>
+    </div>
+    <div class="col-lg-12 pl-3 pr-3">
+        <hr class="bg-white">
+        <p class="text-center text-white font-italic">Chartwell International School, Belgrade, Serbia</p>
     </div>
 </div>
 
