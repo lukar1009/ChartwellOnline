bf70e5d483982878451c7518d41d5d496bd13aed
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-18 15:09:14 +0200

    Home page edited

---
+++ includes/slider.php
@@ -0,0 +1,21 @@
+<div id="carouselExampleFade" class="carousel slide carousel-fade rounded" data-ride="carousel">
+  <div class="carousel-inner rounded">
+    <div class="carousel-item active">
+      <img src="images/slider1.jpg" style="height: 500px;" class="d-block w-100 img-fluid" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider2.jpg" style="height: 500px;" class="d-block w-100 img-fluid" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider3.jpg" style="height: 500px;" class="d-block w-100 img-fluid" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php require "includes/header.php"; ?>
+<?php require "includes/navigation.php"; ?>
+
+<div class="container-fluid">
+
+<div class="row">
+    <div class="col-lg-6 pl-3 pr-3 pt-1">
+        <?php require "includes/slider.php"; ?>
+    </div>
+    <div class="col-lg-6 pt-1">
+        <!-- <h1 class="text-wrap text-center text-light font-weight-light font-italic">Welcome to Chartwell ONLINE CLASSROOM</h1> -->
+        <!-- <h1 class="text-light display-4 text-center font-italic">Welcome to Chartwell ONLINE CLASSROOM</h1>
+        <p class="mt-4 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas obcaecati, similique voluptatum quos, ratione laudantium optio dolor eveniet suscipit provident sit. Ea reiciendis tenetur enim omnis veniam totam amet excepturi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis nulla assumenda, at eum ipsam sit corporis vel fugit odio hic, sint doloribus cum! Modi cum nostrum nobis commodi aut?</p> -->
+        <div class="jumbotron"style="height: 500px;">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require "includes/footer.php"; ?>
+    
+
