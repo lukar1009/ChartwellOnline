fcbf219ce0ca540e2c679fb001a27c32f4de4817
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-19 22:51:23 +0200

    Mailing system enabled.

--- admin/contact_support.php
+++ admin/contact_support.php
@@ -13,6 +13,66 @@
                 </div>
             </div>
             <!-- /.row -->
+            <div class="modal-dialog text-center ">
+                <div class="col-sm-9 main-section">
+                        <h1>E-mail: lukaradovanovic1311@gmail.com</h1>
+                        <h1>Phone: +381 63 7531 200</h1>
+                        <h1>Location: Belgrade, Serbia</h1>
+                    <div class="modal-content scene_element scene_element--fadein">
+                        <?php 
+
+                        if(isset($_GET['success'])){
+                            ?>
+
+                            <div class="alert alert-success" role="alert">
+                                Mail sent!
+                            </div>
+
+                            <?php
+                        }
+
+                        if(isset($_GET['error'])){
+                            ?>
+
+                            <div class="alert alert-danger" role="alert">
+                                Server down, please call support on the number above.
+                            </div>
+
+                            <?php
+                        }
+
+                        if(isset($_GET['empty'])){
+                            ?>
+
+                            <div class="alert alert-danger" role="alert">
+                                No field must be empty, please try again.
+                            </div>
+
+                            <?php
+                        }
+                        
+                        ?>
+
+                        <div class="col-12 form-input">
+                            <form action="./phpmail/index.php" method="post">
+                                <div class="form-group">
+                                    <input id="fullname" name="fullname" type="text" class="form-control" placeholder="Enter Your Name:">
+                                </div>
+                                <div class="form-group">
+                                    <input id="mail" name="mail" type="text" class="form-control" placeholder="Enter Your E-Mail:">
+                                </div>
+                                <div class="form-group">
+                                    <input id="subject" name="subject" type="text" class="form-control" placeholder="Enter Subject:">
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" name="mail_content" id="mail_content" cols="30" rows="10" placeholder="Enter your message:"></textarea>
+                                </div>
+                                <button name="submit" type="submit" class="btn btn-success">Contact me!</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- /.container-fluid -->
     </div>
---
+++ admin/phpmail/index.php
@@ -0,0 +1,50 @@
+<?php 
+
+//parametri za php mejler
+require "phpmailer/PHPMailerAutoload.php";
+$mejl = new PHPMailer();
+$mejl->isSMTP();
+$mejl->SMTPDebug = 3;
+$mejl->isHTML(true);
+$mail->Sendmail = '/usr/sbin/sendmail';
+
+
+if(isset($_POST['submit'])){
+    $fullname = $_POST['fullname'];
+    $user_mail = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $mail_body = $_POST['mail_content'];
+}
+
+//parametri za gmail nalog
+$mejl->Host = 'smtp.gmail.com';
+$mejl->Port = 587;
+$mejl->SMTPAuth = true;
+$mejl->SMTPSecure = 'tls';
+$mejl->Username = "lukaradovanovic1311@gmail.com";
+$mejl->Password = "wt3gj55s";
+
+
+//parametri za slanje mejla
+//1.ko salje
+$mejl->setFrom($user_mail, $fullname);
+// //2.kome stize odgovor
+// $mejl->addReplyTo($user_mail, 'LukaReply');
+//3.gde saljemo, tj korisnikova mejl adresa i ime
+$mejl->addAddress('lukaradovanovic1311@gmail.com');
+
+//kreiramo telo mejla
+$mejl->Subject = $subject;
+$mejl->Body = $mail_body; //ovde mogu ici i html tagovi
+
+//slanje mejla
+if(!$mejl->send()){
+    //echo "Mejl nije poslat" . $mejl->ErrorInfo;
+    header("Location: ../contact_support.php?error=1");
+}else{
+    //echo "Mejl je poslat";
+    header("Location: ../contact_support.php?success=1");
+}
+
+
+?>
