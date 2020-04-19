8db70ea6eed81bc398b56cdede2ee4679bac85e1
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-19 20:09:46 +0200

    View all lessons functionallity added

--- admin/view_all_lessons.php
+++ admin/view_all_lessons.php
@@ -13,6 +13,52 @@
                 </div>
             </div>
             <!-- /.row -->
+            <div class="row " id="main">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">Subject</th>
+                    <th scope="col">Lesson Name</th>
+                    <th scope="col">Lesson Description</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    
+                    $query = "select * from lessons";
+                    $select_lessons_query = mysqli_query($connection, $query);
+                    if(!$select_lessons_query){
+                        die("QUERY1 failed: ". mysqli_error($connection));
+                    }
+                    while($row = mysqli_fetch_assoc($select_lessons_query)){
+                        $lesson_id = $row['id'];
+                        $lesson_name = $row['lesson_name'];
+                        $lesson_desc = $row['lesson_desc'];
+                        
+                        $inner_query = "select * from subjects where id = $lesson_id";
+                        $select_subject = mysqli_query($connection, $inner_query);
+                        if(!$select_subject){
+                            die("QUERY2 failed: ". mysqli_error($connection));
+                        }
+                        $inner_row = mysqli_fetch_assoc($select_subject);
+                        $subject_name = $inner_row['subject_name'];
+
+                        echo "<tr>";
+                        echo    "<td>$subject_name</td>";
+                        echo    "<td>$lesson_name</td>";
+                        echo    "<td>$lesson_desc</td>";
+                        echo    "<td><a href='#'>Edit</a></td>";
+                        echo    "<td><a href='#'>Delete</a></td>";
+                        echo "</tr>";
+                    }
+                    
+                    ?>
+
+                </tbody>
+            </table>
+        </div>
         </div>
         <!-- /.container-fluid -->
     </div>
