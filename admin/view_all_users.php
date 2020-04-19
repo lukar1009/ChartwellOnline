b184e61d13893fe5414f8ad31da757a414d6843a
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-19 19:57:27 +0200

    added view users page in admin

--- admin/view_all_users.php
+++ admin/view_all_users.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         <!-- /.row -->
-        <div class="row" id="main">
+        <div class="row " id="main">
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -24,30 +24,31 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <td>@mdo</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <td>@mdo</td>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
+                    <?php 
+                    
+                    $query = "select * from users where user_role = 'student'";
+                    $select_students_query = mysqli_query($connection, $query);
+                    if(!$select_students_query){
+                        die("QUERY2 failed: ". mysqli_error($connection));
+                    }
+                    while($row = mysqli_fetch_assoc($select_students_query)){
+                        $user_firstname = $row['user_firstname'];
+                        $user_lastname = $row['user_lastname'];
+                        $user_email = $row['user_email'];
+                        $username = $row['username'];
+                        
+                        echo "<tr>";
+                        echo    "<td>$user_firstname</td>";
+                        echo    "<td>$user_lastname</td>";
+                        echo    "<td>$user_email</td>";
+                        echo    "<td>$username</td>";
+                        echo    "<td><a href='#'>Edit</a></td>";
+                        echo    "<td><a href='#'>Delete</a></td>";
+                        echo "</tr>";
+                    }
+                    
+                    ?>
+
                 </tbody>
             </table>
         </div>
@@ -64,35 +65,46 @@
                     <th scope="col">Lastname</th>
                     <th scope="col">Email</th>
                     <th scope="col">Username</th>
+                    <th scope="col">Subject</th>
                     <th scope="col">Edit</th>
                     <th scope="col">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <td>@mdo</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <td>@mdo</td>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
+                <?php 
+                    
+                    $query = "select * from users where user_role = 'teacher'";
+                    $select_students_query = mysqli_query($connection, $query);
+                    if(!$select_students_query){
+                        die("QUERY1 failed: ". mysqli_error($connection));
+                    }
+                    while($row = mysqli_fetch_assoc($select_students_query)){
+                        $user_id = $row['user_id'];
+                        $user_firstname = $row['user_firstname'];
+                        $user_lastname = $row['user_lastname'];
+                        $user_email = $row['user_email'];
+                        $username = $row['username'];
+                        
+                        $inner_query = "select subject_name from subjects inner join teacher_subject on subjects.id = teacher_subject.subject where teacher_subject.user_id = $user_id";
+                        $subject_query = mysqli_query($connection, $inner_query);
+                        if(!$subject_query){
+                            die("QUERY3 failed: ". mysqli_error($connection));
+                        }
+                        $inner_row = mysqli_fetch_assoc($subject_query);
+                        $teacher_subject = $inner_row['subject_name'];
+
+                        echo "<tr>";
+                        echo    "<td>$user_firstname</td>";
+                        echo    "<td>$user_lastname</td>";
+                        echo    "<td>$user_email</td>";
+                        echo    "<td>$username</td>";
+                        echo    "<td>$teacher_subject</td>";
+                        echo    "<td><a href='#'>Edit</a></td>";
+                        echo    "<td><a href='#'>Delete</a></td>";
+                        echo "</tr>";
+                    }
+                    
+                    ?>
                 </tbody>
             </table>
     </div>
