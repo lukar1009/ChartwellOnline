19d97bd204b0f2b4b63d4c79fb752f58977c39a9
lukar1009 <lukaradovanovic1311@gmail.com>
2020-04-21 12:39:56 +0200

    Showing lessons from database enabled

--- year_pages/lesson_view.php
+++ year_pages/lesson_view.php
@@ -1,12 +1,20 @@
 <?php require "./includes/year_header.php"; ?>
 <?php require "./includes/year_navigation.php"; ?>
 
+<div class="container-fluid">
 
+<div class="row scene_element scene_element--fadeinright">
+    <p class="display-3 col-12 text-center text-white">View here or read more!</p>
+</div>
+<div class="row mb-3 scene_element scene_element--fadeinup">
 <?php 
 
 if(isset($_GET['year']) && isset($_GET['sub'])){
     $year = $_GET['year'];
     $sub_tag = $_GET['sub'];
+
+    $year = mysqli_real_escape_string($connection, $year);
+    $sub_tag = mysqli_real_escape_string($connection, $sub_tag);
     //pretraga po tagu
     //Pretrazivanje sadrzaja po godini iz lesson tabele i subject iz subject tabele!!!
     $subject_id_query = "select id from subjects where subject_tag = '$sub_tag'";
@@ -30,26 +38,38 @@ if(isset($_GET['year']) && isset($_GET['sub'])){
     //taj novi div ostaje istih karakteristika osim sto se tekst menja, kao i img u video
     //podesiti karakteristike videa da moze da se prikaze(naziv iz baze ide u putanju do videa!!)
     //i prikazivati u panel div-u u odredjenim poljima
+
+    while($row = mysqli_fetch_assoc($select_lesson)){
+        $lesson_name = $row['lesson_name'];
+        $lesson_video = $row['lesson_video']
+
+
+    ?>
+        <div class="col-lg-4 col-md-6 text-center mb-3">
+            <div class="panel bg-light pt-4 pb-4 pl-2 pr-2 rounded">
+                <div class="panel-body">
+                    <!-- <img style="height: 400px; width: 500px;" src="./images/english.png" alt="" class="img-responsive img-thumbnail"> -->
+                    <!-- <video controls>
+                        <source src="./upload/<?php //echo $row['video_name'] ?>" type="video/webm">
+                        Your browser does not support this type of video.
+                    </video> -->
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <video controls="true" class="embed-responsive-item" src="./upload/<?php echo $lesson_video; ?>" allowfullscreen></video>
+                    </div>
+                    <h4><?php echo $lesson_name; ?></h4>
+                    <button class="btn btn-danger"><a class="nav-link text-white" href="#">Read more...</a></button>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
 }
 
 ?>
-<div class="container-fluid">
+    </div><!--ROW END!!! -->
 
-
-<div class="row scene_element scene_element--fadeinright">
-    <p class="display-3 col-12 text-center text-white">View here or read more!</p>
-</div>
-<div class="row mb-3 scene_element scene_element--fadeinup">
-    <div class="col-lg-4 col-md-6 text-center mb-3">
-        <div class="panel bg-light pt-4 pb-4 pl-2 pr-2 rounded">
-            <div class="panel-body">
-                <img style="height: 400px; width: 500px;" src="./images/english.png" alt="" class="img-responsive img-thumbnail">
-                <h4>English Language</h4>
-                <button class="btn btn-danger"><a class="nav-link text-white" href="#">Read more...</a></button>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-md-6 text-center mb-3">
+    <!-- TEST LESSONS FOR FRONT END DESIGN -->
+    <!-- <div class="col-lg-4 col-md-6 text-center mb-3">
         <div class="panel bg-light pt-4 pb-4 pl-2 pr-2 rounded">
             <div class="panel-body">
                 <img style="height: 400px; width: 500px;" class="img-responsive img-thumbnail" src="./images/math.jpg" alt="">
@@ -95,8 +115,8 @@ if(isset($_GET['year']) && isset($_GET['sub'])){
                 <button class="btn btn-danger"><a class="nav-link text-white" href="#">Read more...</a></button>
             </div>
         </div>
-    </div>
-</div>
+    </div> -->
+
 </div>
 
 <?php require "./includes/year_footer.php"; ?>
